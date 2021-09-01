93a7f4b811eb5d1ac3e61e48fd3c7f129951a611
Christopher Kang <42285636+christopherkang@users.noreply.github.com>
2021-09-01 08:21:54 -0700

    Update testbme.py

--- testbme.py
+++ testbme.py
@@ -1,6 +1,8 @@
 import smbus2
 import bme280
+import numpy as np
 
+## demo usage
 port = 1
 address = 0x76
 bus = smbus2.SMBus(port)
@@ -9,9 +11,15 @@ calibration_params = bme280.load_calibration_params(bus, address)
 
 data = bme280.sample(bus, address, calibration_params)
 
+print("Initializing sensor")
+print("Testing out sample readings")
+
 print(data.id)
 print(data.timestamp)
 print(data.temperature)
 print(data.pressure)
 print(data.humidity)
 print(data)
+
+
+
